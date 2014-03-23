4721ee677977df781d6f0650e4febd70b109406e
coburn berry <coburn.d.berry@gmail.com>
2014-03-23 02:21:43 -0700

    product show pages come as modals

--- app/assets/javascripts/views/farmers/farmer_show.js
+++ app/assets/javascripts/views/farmers/farmer_show.js
@@ -8,6 +8,16 @@ Market.Views.FarmerShow = Backbone.View.extend({
     this.listenTo(this.farmer, 'sync', this.render);
   },
 
+  events: {
+    "click button.show-product":"showProduct"
+  },
+
+  showProduct: function(){
+    var id = $(event.target).data('id')
+    var psView = new Market.Views.ProductShow({ id: id});
+    $('div.modal-body').html(psView.render().$el)
+  },
+
   template: JST['farmers/show'],
 
   render: function(){
