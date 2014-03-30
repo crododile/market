0a1fb30a94a69e2342b37a82bee23421dcdcfe02
ridingrails <student@appacademy.io>
2014-03-29 21:10:21 -0700

    add product is a dropdown

--- app/assets/javascripts/views/products/products_new.js
+++ app/assets/javascripts/views/products/products_new.js
@@ -10,7 +10,7 @@ Market.Views.ProductsNew = Backbone.View.extend({
   render: function(){
     var productT = new Market.Models.ProductType();
 
-    var rc = this.template( { productT: productT });
+    var rc = this.template( { pTs: this.collection, productT: productT });
     this.$el.html(rc);
     return this;
   },
